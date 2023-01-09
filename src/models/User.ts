6cbe45f9705f722b685cca38753e2af398228cbb
Malak Joseph <malakjoseph2010@gmail.com>
2023-01-09 05:15:28 +0200

    fetch all data on userByID

--- src/models/User.ts
+++ src/models/User.ts
@@ -38,7 +38,7 @@ export class User {
   async getUserById(userID: UserType["id"]): Promise<Record<"id", number>> {
     try {
       const conn = await pool.connect();
-      const sql = "SELECT id FROM users WHERE id=$1";
+      const sql = "SELECT * FROM users WHERE id=$1";
       const result = await conn.query<Record<"id", number>>(sql, [userID]);
 
       conn.release();
