b65d16b6d2a749bfdecffc5e41cb6e5399fab5df
Malak Joseph <malakjoseph2010@gmail.com>
2023-01-10 04:30:10 +0200

    User handler tests

--- src/tests/handlers/users.spec.ts
+++ src/tests/handlers/users.spec.ts
@@ -4,9 +4,28 @@ import app from "../../server";
 
 const request = supertest(app);
 
+const userID = "1";
+let userToken = "Bearer ";
+
 describe("User Handler Suite", () => {
   it("Should create a user sucessfully", async () => {
     const result = await request.post("/users").send(userCredentials);
+    userToken += result.body;
+
     expect(result.statusCode).toBe(201);
+  });
+
+  it("Should fetch all users sucessfully", async () => {
+    const result = await request.get("/users").set("Authorization", userToken);
+
+    expect(result.statusCode).toBe(200);
+  });
+
+  it("Should fetch user by id sucessfully", async () => {
+    const result = await request
+      .get(`/users/${userID}`)
+      .set("Authorization", userToken);
+
+    expect(result.statusCode).toBe(200);
   });
 });
