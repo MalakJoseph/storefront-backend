61ee0a40959e2b5a20051d274c3804c7b794a207
Malak Joseph <malakjoseph2010@gmail.com>
2023-01-10 07:25:22 +0200

    test environment

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,22 @@ import { Pool } from "pg";
 
 dotenv.config();
 
+const {
+  NODE_ENV,
+  DB_HOST,
+  DB_NAME,
+  DB_NAME_TEST,
+  DB_USERNAME,
+  DB_PASSWORD,
+  PORT,
+} = process.env;
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.PORT as string),
+  host: DB_HOST,
+  database: NODE_ENV === "test" ? DB_NAME_TEST : DB_NAME,
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  port: parseInt(PORT as string),
 });
 
 export default pool;
