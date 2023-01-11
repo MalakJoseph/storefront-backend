85ac65a59fa55ae0bb9f555b405f5ff51070b663
Malak Joseph <malakjoseph2010@gmail.com>
2023-01-11 07:20:10 +0200

    fix orders up migration

--- migrations/sqls/20230107042252-orders-table-up.sql
+++ migrations/sqls/20230107042252-orders-table-up.sql
@@ -1,9 +1,6 @@
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY,
-  product_id INTEGER,
-  quantity INTEGER DEFAULT 1,
-  user_id INTEGER,
   status VARCHAR(20),
-  FOREIGN KEY (product_id) REFERENCES products(id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  user_id INTEGER REFERENCES users(id) NOT NULL ON DELETE CASCADE,
+  created_at timestamp default current_timestamp
 );
